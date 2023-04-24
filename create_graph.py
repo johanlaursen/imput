48de070c8b4fa65ee4c85a49f51b955d7210f835
Johan Laursen <johan7laursen@gmail.com>
2023-04-24 11:58:26 +0200

    add create_graph

--- create_graph.py
+++ create_graph.py
@@ -3,19 +3,22 @@ import networkx as nx
 import pickle
 
 
+def main():
+    path = "data/gtfs_rejsekort.zip"
 
-path = "data/gtfs_rejsekort.zip"
+    feed = pt.get_representative_feed(path)
+    # start and end are used to only select part of the data
+    start = 0*60*60  # 0:00
+    end = 36*60*60  # 36:00 because data is retarded
 
-feed = pt.get_representative_feed(path)
-# start and end are used to only select part of the data
-start = 0*60*60  # 0:00
-end = 24*60*60  # 24:00
+    G = pt.load_feed_as_graph(feed, start, end)
 
-G = pt.load_feed_as_graph(feed, start, end)
+    # Relabels nodes by removing 04LSS_ from each node
+    relabel_dic = {node:node[6:] for node in G.nodes()}
+    G = nx.relabel_nodes(G, relabel_dic, copy=False)
 
-# Relabels nodes by removing 04LSS_ from each node
-relabel_dic = {node:node[6:] for node in G.nodes()}
-G = nx.relabel_nodes(G, relabel_dic, copy=False)
+    with open("rejsekort_graph_cleaned.gpickle", "wb") as f:
+        pickle.dump(G, f)
 
-with open("rejsekort_graph_cleaned.gpickle", "wb") as f:
-    pickle.dump(G, f)
+if __name__ == "__main__":
+    main()
